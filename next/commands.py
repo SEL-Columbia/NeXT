5da1a3392b2ad62af671783d6d27bdeda6385073
Ivan Willig <iwillig@gmail.com>
2011-09-08 17:26:35 -0400

    Finalized exporting SA tables to yaml. Working on importing now.

--- next/commands.py
+++ next/commands.py
@@ -19,7 +19,7 @@ handler.setFormatter(formatter)
 
 logger.addHandler(handler)
 
-
+geom_types = ('POINT', 'GEOMETRY')
 fix_folder = 'fixtures'
 
 
@@ -32,10 +32,21 @@ class ImportFixtures(Command):
                       help='')
 
     def command(self):
+        from yaml import load
+        from next.models import Base
         config_uri = self.args[0]
         env = bootstrap(config_uri)
         engine = engine_from_config(env['registry'].settings, 'sqlalchemy.')
         initialize_sql(engine)
+        tables = Base.metadata.tables
+        for table_name in tables:
+            table = tables[table_name]
+            yaml_data = load(open('%s/%s.yaml' % (fix_folder, table_name)))
+            logger.info('Loading data for %s' % table_name)
+            for obj in yaml_data:
+                # sanity check before we import the data
+                assert len(obj) == len(table.c.keys())
+                table.insert(obj.values()).execute()
 
 
 class ExportFixtures(Command):
@@ -49,6 +60,7 @@ class ExportFixtures(Command):
     def command(self):
         from next.models import Base
         from shapely.wkb import loads
+        from yaml import dump
         config_uri = self.args[0]
         env = bootstrap(config_uri)
         engine = engine_from_config(env['registry'].settings, 'sqlalchemy.')
@@ -56,20 +68,27 @@ class ExportFixtures(Command):
 
         tables = Base.metadata.tables
         for table_name in tables.keys():
+            yaml_file = open('%s/%s.yaml' % (fix_folder, table_name), 'w')
+            table_data = []
             table = tables[table_name]
 
-            #logger.info('Exporing table --> %s' % table)
+            logger.info('Exporing table -> %s' % table)
+            logger.info('Table columns  -> %s ' % table.c.keys())
 
             stmt = table.select()
             for row in stmt.execute():
                 c = {}
                 columns = table.c.keys()
+                # sanity check before we export the data
                 assert len(columns) == len(row)
                 for i in range(len(columns)):
                     column = table.c[columns[i]]
                     cell = row[i]
-                    if str(column.type) == 'POINT':
+                    if str(column.type) in geom_types:
+                        # we have to call str on the binary column first
                         c[column.name] = loads(str(cell)).wkt
                     else:
                         c[column.name] = cell
-                print c
+                table_data.append(c)
+            dump(table_data, yaml_file)
+            yaml_file.close()
