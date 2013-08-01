8fd2c7837a0e841105e8c5187277e489c51e45a2
chrisnatali <chris.natali@gmail.com>
2013-08-01 21:41:55 +0000

    db name is now 'next' and sql is now loaded in fixed order

--- drop-and-create.sh
+++ drop-and-create.sh
@@ -8,7 +8,7 @@ for file in $sql_load_list;
 do 
   file_path=`find /usr/share/postgresql/9.1/contrib/postgis-2.0 -name $file`
   if [ -f "$file_path" ]; then
-    psql -U postgres -d $1 -f "$file_path"
+    psql -U next -d $1 -f "$file_path"
   else
     echo "$file not found, check your postgis installation."
     return -1
