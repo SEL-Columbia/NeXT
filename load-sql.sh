4906ebb21c86645c1a5d635f53b84f61fc3c837f
chrisnatali <chris.natali@gmail.com>
2013-08-02 20:18:01 +0000

    added phases to load install

--- load-sql.sh
+++ load-sql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-files="db/base.sql db/0_base.sql db/1_base.sql db/9_nodes_from_phase.sql"
+files="db/base.sql db/0_base.sql db/1_base.sql db/2_phases.sql db/9_nodes_from_phase.sql"
 for file in $files;
   do psql -d $1 -f $file
 done
